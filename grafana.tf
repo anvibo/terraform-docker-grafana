0f9a8ce2f633394a09f1cb730cb93f938f9a230a
Bogdan <bfrunza@anvibo.com>
2019-03-19 17:35:11 +0200

    adding files

---
+++ grafana.tf
@@ -0,0 +1,36 @@
+
+resource "docker_volume" "grafana_data" {
+  name = "grafana_data"
+  driver = "local-persist"
+  driver_opts = {
+      "mountpoint" = "${var.vol1_mountpoint}"
+  }
+}
+resource "docker_service" "grafana" {
+    name = "grafana-service"
+
+    task_spec {
+        container_spec {
+            image = "grafana/grafana"
+
+            labels {
+                traefik.frontend.rule = "Host:${var.url}"
+                traefik.port = 3000
+                traefik.docker.network = "${var.traefik_network}"
+            }
+
+         
+
+            mounts = [
+                {
+                    source      = "${docker_volume.grafana_data.name}"
+                    target      = "/var/lib/grafana"   
+                    type        = "volume"
+                    read_only   = false
+                },
+                
+            ]
+        }
+        networks     = ["${var.networks}"]
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "networks" {
+  type = "list"
+}
+
+variable "traefik_network" {
+}
+variable "url" {
+  
+}
+variable "vol1_mountpoint" {
+  
+}
